e23abf679977179406a37f0039587206f4352ab2
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2022-11-30 19:46:46 +0200

    frist js task

---
+++ 5 ITI Aswan/javascript/Day 1/day1/day1/Task 1/index.js
@@ -0,0 +1,115 @@
+// ############# task 1
+
+// var tempreture = parseInt(prompt("Enter temp"))
+//  console.log(tempreture);
+//     let res = tempreture >= 30 ? "Hot" : "cold";
+//     alert(res);
+  
+
+
+// ############### Task 2#################
+
+// var user = (prompt("Enter name"));
+
+// if(isNaN (user)){
+// console.log (typeof(user));
+// }
+
+// else {
+//   isFinite()
+//   console.log("مش كده ي عم ")
+// }
+
+
+// var birth = (prompt("Enter birth"));
+
+// if (isFinite(birth) && birth < 2010 ) {
+//   console.log("test")
+
+//   var age = 2022-birth
+//   console.log(age)
+
+ 
+
+// }
+// else{
+//   console.log("دخل عمرك ارقام")
+
+//   for (var x = 0 ; x <3 ; x++ )
+//   {
+//     var birth = (prompt("Enter birth"));
+//     console.log("Enter your age rigt")
+
+//     if(isFinite(birth) && (birth < 2010)){
+//       console.log(x)
+//       x=3;
+//     }
+
+   
+//   }
+
+//   document.getElementById("name").innerHTML= `name : ${user}`;
+    
+//   document.getElementById("birth").innerHTML=`birth : ${birth}`;
+  
+//   document.getElementById("age").innerHTML=`age : ${age}`;
+
+
+// }
+
+
+// // ############ Question 3 #############
+// let root1 , root2
+// let a = parseInt(prompt("Enter A"))
+// let b = parseInt(prompt("Enter B"))
+// let c = parseInt(prompt("Enter C"))
+
+// root1 = -b/(2*a) - (Math.sqrt *b^2 - 4 * a * c)
+// console.log(root1)
+// root2 = -b/(2*a) + (Math.sqrt *b^2 - 4 * a * c)
+// console.log(root2)
+
+
+
+
+
+
+
+// ################question 4 ####################
+// let sum
+
+// let num1 =parseInt (prompt("Enter frist num"))%10
+// let num2 = parseInt(prompt("Enter second num"))%10
+
+// sum = (num1+num2)
+// console.log(sum)
+
+
+
+
+
+// ################ Question 5###################
+// var distance = parseInt(prompt("Enter the distance in km"))
+// var time = distance*2
+
+// alert(time +"  " +"muinites")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
