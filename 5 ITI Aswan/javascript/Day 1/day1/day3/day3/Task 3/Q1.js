a2f1e209267c54c7f19c454947352c0ee480007a
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2022-12-04 17:34:29 +0200

    commit task 3

---
+++ 5 ITI Aswan/javascript/Day 1/day1/day3/day3/Task 3/Q1.js
@@ -0,0 +1,126 @@
+// document.getElementById('bt1').addEventListener("click" , opchild)
+// document.getElementById('bt2').addEventListener("click" , clochild)
+
+// var linkG = 'https://www.w3schools.com/jsref/met_win_open.asp'
+// let myWimndow
+// function opchild(){
+//     myWimndow =  window.open(linkG , "_blank", "width=500, height=500")
+
+//     console.log("open")
+// }
+
+// function clochild(){
+//     myWimndow.close()
+
+// }
+
+
+
+            //Question 2
+
+
+// document.getElementById('alerClock').addEventListener("click" ,displayClock  )
+// document.getElementById('alerClock').addEventListener("click" ,alert2  )
+
+
+// function time(){
+//     var d = new Date();
+//     var s = d.getSeconds();
+//     var m = d.getMinutes();
+//     var h = d.getHours();
+//     document.write(h + " : " + m + " :  " + s + " $ ");
+// }
+
+// setInterval(time,1000);
+                             //  حل أخر
+
+// function alert2(){
+//     alert("Start Clock")
+
+// }
+// function displayClock(){
+//   var date = new Date().toLocaleTimeString();
+//   document.body.innerHTML = date;
+//   }
+//   setInterval(displayClock, 1000)
+
+
+
+
+            //Question 3
+
+// var btnOpen = document.getElementById('btn3');
+// var btnClose = document.getElementById('btn4');
+// btnOpen.addEventListener("click",openWindow);
+// btnClose.addEventListener("click",closeWindow)
+// let myWindow;
+// function openWindow(){
+//     myWindow = window.open("", "_blank", "width=500, height=500");
+    
+
+// }
+// function type(){
+//     var text = "AtaAlla Ibrahim Atalla";
+//     var i;
+//     var o = "";
+//     for(i = 0;i < text.length;i++){
+//     o += text[i]
+//     document.write(o[i])
+    
+//     }
+// }
+// myWindow.setTimeout(openWindow,3000);
+
+// myWindow.setInterval(type(), 1000);
+// function closeWindow(){
+//     myWindow.close();
+// }
+
+
+
+
+
+          //Question 4
+        //   window.addEventListener("scroll" , function scorling()
+        //   {
+        //       if(scrollY>777)
+        //       {
+        //          this.alert("Hi")
+        //       }
+        //   }
+        //   )
+          
+
+
+
+          // Question 5
+
+            // document.getElementById('text1').addEventListener("input" , typeChild)
+            // var tyChild 
+            // function typeChild(){
+
+
+            //     tyChild= window.open('',"_blank", "width=500, height=500" )
+            //     tyChild.document.write("Hello to my new window")
+
+            // }
+            // setTimeout(function(){
+            //     tyChild.close()
+            // } , 5000)
+
+
+
+            // Question 6
+
+            //  document.getElementById('opScroll').addEventListener("click" ,ad )
+            //  var ad1
+            //  function ad(){
+            //      ad1= window.open('' , "_blank", "width=500, height=500")
+            //      ad1.window.scrollTo(1000, 2000)
+ 
+            //  }
+
+
+
+
+  
