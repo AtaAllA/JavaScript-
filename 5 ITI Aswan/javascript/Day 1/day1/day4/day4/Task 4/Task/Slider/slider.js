6826e4e668ee1226d3685a3cddbeb3378f2a5de6
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2022-12-06 22:20:29 +0200

    commit

--- 5 ITI Aswan/javascript/Day 1/day1/day4/day4/Task 4/Task/Slider/slider.js
+++ 5 ITI Aswan/javascript/Day 1/day1/day4/day4/Task 4/Task/Slider/slider.js
@@ -1,64 +1,64 @@
-var images=["imgs/SlideShow/1.jpg","imgs/SlideShow/2.jpg","imgs/SlideShow/3.jpg","imgs/SlideShow/4.jpg","imgs/SlideShow/5.jpg"];
-var i=0;
-var img=document.getElementById("img");
-var pic=0;
+var images = ["imgs/SlideShow/1.jpg", "imgs/SlideShow/2.jpg", "imgs/SlideShow/3.jpg", "imgs/SlideShow/4.jpg", "imgs/SlideShow/5.jpg"];
+var i = 0;
+var img = document.getElementById("img");
+var pic = 0;
 
 
 
-function Next(){
+function Next() {
+
 
     i++;
-var pic=setInterval(function(){
 
-    img.src= images[i];
- 
-},1000);
+    img.src = images[i];
 
 
-if(i=images.length-1)
-{
-   clearInterval(pic) ;
-}
 
 
-  
+    if (i = images.length - 1) {
+        img.src = "6.jpg";
+    }
+
 
 }
 
-function previous(){
+function previous() {
     i--;
 
-    var pic=setInterval(function(){
-        img.src= images[i];
-     
-    },1000);
-    
-    if(i[0])
-    {
-       clearInterval(pic) ;
+
+    img.src = images[i];
+
+
+
+    if (i = 0) {
+        img.src = "1.jpg";
     }
 
 }
+var pic = 0;
+function play() {
+    if (i = images.length - 1) {
+        i = 0;
 
-function play(){
-    if(i=images.length-1)
-    {i=0;
+        pic = setInterval(function () {
 
-        var pic=setInterval(function(){
+            img.src = images[i];
             i++;
-            img.src= images[i];
-         
-        },1000);
-        
-        if(i[0])
-        {
-           clearInterval(pic) ;
-        }
-    
+        }, 1000)
     }
+
+
+    else if (i = 0) {
+        clearInterval(pic);
+
+
+    }
+
+
 
 }
 
-function stop(){
-    clearInterval(pic) ;
+
+function stop() {
+    clearInterval(pic);
 }
