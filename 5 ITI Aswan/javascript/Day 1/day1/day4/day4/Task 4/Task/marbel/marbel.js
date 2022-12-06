2dcc6954f6e678a25c89ead8fb14ef524032a3de
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2022-12-06 22:41:26 +0200

    commit

--- 5 ITI Aswan/javascript/Day 1/day1/day4/day4/Task 4/Task/marbel/marbel.js
+++ 5 ITI Aswan/javascript/Day 1/day1/day4/day4/Task 4/Task/marbel/marbel.js
@@ -1,23 +1,56 @@
 var imges=["imgs/marble2.jpg","imgs/marble3.jpg"]
 var imgs=document.getElementsByTagName("img");
-var i=0;
 
 var y=0;
-function replace(){
+
+
+setInterval(function replace(){
    
-    i++;
+    for(i = 0 ; i<imges.length ; i++){
+
         imgs[y].src=imges[i];
+        
         if(i>1)
         {
             i=0
         }
         y++
+
+    }
+        
     
     
     }
+,1000)
 
 
-setInterval(replace,1000)
+
+
+
+////////////////////////////////  حل اخر/////////////////////////////// 
+// var imges=["imgs/marble2.jpg","imgs/marble3.jpg"]
+// var imgs=document.getElementsByTagName("img");
+// var i=0;
+
+// var y=0;
+
+// setInterval(function replace(){
+   
+    
+// i++
+//         imgs[y].src=imges[i];
+//         if(i>1)
+//         {
+//             i=0
+//         }
+//         y++
+
+    
+        
+    
+    
+//     }
+// ,1000)
 
 
 
